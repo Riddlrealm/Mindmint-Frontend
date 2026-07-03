5e383bd84c90efb098d6bb3612314856840bca3f
7udah <merlik787@gmail.com>
2026-07-03 14:02:07 +0000

    AppRoutes: collapse JSX, destructure route shape in the map

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,21 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { routeConfig } from "../config/routeConfig";
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      {routeConfig.map((route) => {
-        const Component = route.element;
-        return (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={<Component />}
-          />
-        );
-      })}
-    </Routes>
-  );
-};
+const AppRoutes = () => (
+  <Routes>
+    {routeConfig.map(({ path, element: Component }) => (
+      <Route key={path} path={path} element={<Component />} />
+    ))}
+  </Routes>
+);
 
 export default AppRoutes;

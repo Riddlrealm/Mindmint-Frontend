6736ac018915e27ddb40540973e51f411ad06d48
7udah <merlik787@gmail.com>
2026-07-03 12:15:11 +0000

    WhyShouldYouPlaySection: replace arbitrary Tailwind values with standard scale

--- src/components/why-should-you-play-section.tsx
+++ src/components/why-should-you-play-section.tsx
@@ -5,27 +5,27 @@ const WhyShouldYouPlaySection = () => {
         <img src="/images/quest-bg.png" alt="why-should-you-play" />
       </div>
 
-      <div className="lg:pr-19 py-15 lg:py-50 max-w-229 lg:w-[70%]">
-        <h2 className="font-medium text-[45px] pb-5.5">Why Should You Play?</h2>
+      <div className="lg:pr-20 py-16 lg:py-20 max-w-3xl lg:w-[70%]">
+        <h2 className="font-medium text-4xl lg:text-5xl pb-6">Why Should You Play?</h2>
 
-        <div className="space-y-7 ">
-          <p className="font-light text-[31px]">
-            <strong className="font-medium text-[38px] block">
+        <div className="space-y-8 ">
+          <p className="font-light text-xl">
+            <strong className="font-medium text-3xl block">
               &nbsp; &bull; Learn While You Play:
             </strong>
             Enhance your knowledge in various subjects, from history to science
             and pop culture.
           </p>
-          <p className="font-light text-[31px]">
-            <strong className="font-medium text-[38px] block">
+          <p className="font-light text-xl">
+            <strong className="font-medium text-3xl block">
               &nbsp; &bull; Compete with Friends:
             </strong>
             Invite friends to join you in Multiplayer mode and see who can
             achieve the highest score!
           </p>
 
-          <p className="font-light text-[31px]">
-            <strong className="font-medium text-[38px] block">
+          <p className="font-light text-xl">
+            <strong className="font-medium text-3xl block">
               &nbsp; &bull; Flexible Play Options:
             </strong>
             Whether you want to practice at your own pace or compete against
